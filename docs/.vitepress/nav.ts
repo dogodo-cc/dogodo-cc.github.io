f7c71614dfb013d5e59fa21679fa932e94ec28a3
袁炜海 <yuanweihai25@163.com>
2024-06-08 23:44:34 +0800

    docs

--- docs/.vitepress/nav.ts
+++ docs/.vitepress/nav.ts
@@ -2,7 +2,13 @@ import type { DefaultTheme } from 'vitepress';
 
 export function getArticles(): DefaultTheme.SidebarItem[] {
     return [
-        { text: '2024', items: [{ text: '我和黑莓', link: '/article/2024/blackberry.md' }] },
+        {
+            text: '2024',
+            items: [
+                { text: '我和黑莓', link: '/article/2024/blackberry.md' },
+                { text: '骑行知识', link: '/article/2024/bicycle/index.md' },
+            ],
+        },
         { text: '2014', items: [{ text: '小小出租屋', link: '/article/2014/小小出租屋.md' }] },
         {
             text: '2013',
@@ -49,14 +55,21 @@ export function getWorks(): DefaultTheme.SidebarItem[] {
         },
         {
             text: '最佳实践',
-            items: [
-                { text: '埋点', link: '/work/pratices/埋点.md' },
-                { text: '软连接', link: '/work/pratices/symlink/index.md' },
-            ],
+            items: [{ text: '埋点', link: '/work/pratices/埋点.md' }],
         },
         {
-            text: '设计文档',
-            items: [{ text: '短链接', link: '/work/design/short-url/index.md' }],
+            text: 'COCOS',
+            link: '/work/cocos/index.md',
+            items: [
+                { text: '软连接', link: '/work/cocos/symlink/index.md' },
+                { text: '命名规范', link: '/work/cocos/命名规范.md' },
+                { text: '项目工程之断舍离', link: '/work/cocos/项目工程之断舍离.md' },
+                { text: 'web-components', link: '/work/cocos/web-components.md' },
+                { text: 'GitHub名称映射', link: '/work/cocos/github-ids/index.md' },
+                { text: '短链接', link: '/work/cocos/short-url/index.md' },
+                { text: 'headless', link: '/work/cocos/headless/index.md' },
+                { text: '弃用css预处理语言', link: '/work/cocos/弃用css预处理语言.md' },
+            ],
         },
     ];
 }
---
+++ docs/work/cocos/index.md
@@ -0,0 +1,26 @@
+<div class="cocos-page">
+    <a href="https://www.cocos.com/" target="_blank"><img class="cocos-logo" src="./assets/cocos-logo.png" /></a>
+    
+<div class="years">
+    <img src="./assets/year1.jpeg" />
+    <img src="./assets/year2.jpeg" />
+</div>  
+</div>
+
+<style>
+    .cocos-page {
+        & .cocos-logo {
+            width: 100px;
+            margin: 0 auto 30px;
+        }
+        & .years {
+            display: flex;
+            width: 100%;
+            overflow-x: auto;
+            & img {
+                width: 200px;
+            }
+        }
+    }
+
+</style>
