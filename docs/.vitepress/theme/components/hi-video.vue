19074b9f6662e52c56a5f9b5ac3f89643df7a47d
袁炜海 <yuanweihai25@163.com>
2024-12-26 22:36:35 +0800

    publish:docs

---
+++ docs/.vitepress/theme/components/hi-video.vue
@@ -0,0 +1,54 @@
+<template>
+    <div
+        :class="{
+            'hi-video': true,
+            'hi-video--playing': isPlaying,
+        }"
+    >
+        <video controls :src="src" @play="onPlay" @pause="onPause"></video>
+    </div>
+</template>
+
+<script setup>
+// 移动端视频不会提前加载，所以需要一个默认图片，否则黑漆漆的很丑
+import { toRefs, ref } from 'vue';
+const props = defineProps({
+    src: String,
+});
+const { src } = toRefs(props);
+
+const isPlaying = ref(false);
+
+function onPlay() {
+    isPlaying.value = true;
+}
+
+function onPause() {
+    isPlaying.value = false;
+}
+</script>
+<style>
+@media (max-width: 750px) {
+    .hi-video {
+        position: relative;
+        display: inline-flex;
+        &::after {
+            content: '';
+            display: block;
+            position: absolute;
+            inset: 0;
+            background-image: url('https://weihai.dogodo.cc/assets/2.DyYdrqIQ.jpeg');
+            background-size: cover;
+            background-repeat: no-repeat;
+            pointer-events: none;
+            background-position: center;
+            opacity: 0.2;
+        }
+        &.hi-video--playing {
+            &::after {
+                display: none;
+            }
+        }
+    }
+}
+</style>
